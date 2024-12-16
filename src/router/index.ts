e8a6ac4ea30998a2075815e595e10983f2df3079
Ilhomjon <futurewithme1221@gmail.com>
2024-12-16 14:00:27 +0500

    style: Created complete task ui

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AboutView from '../views/AboutView.vue'
+import MainView from '../views/MainView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: AboutView,
+      redirect: '/main'
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/main',
+      name: 'main',
+      component: MainView,
+    },
+    {
+      path: '/completed',
+      name: 'completed',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/CompletedView.vue'),
     },
   ],
 })
---
+++ src/views/CompletedView.vue
@@ -0,0 +1,87 @@
+<script setup>
+   import Todo from '../components/Todo.vue'
+   import Card from 'primevue/card';
+   import DataTable from 'primevue/datatable';
+   import Column from 'primevue/column';
+   import Button from 'primevue/button';
+   import InputText from 'primevue/inputtext';
+
+   import Checkbox from 'primevue/checkbox';
+
+   import {ref, computed} from 'vue';
+   const todos = ref([{name: 'Coding'}]);
+   const editTaskText = ref('');
+   const searchValue = ref('');
+
+   const handleAddTodo = (value) => {
+        if(editTaskText.value.length) {
+          todos.value = todos.value.map((todo) => todo.name === editTaskText.value ? value : todo);
+          editTaskText.value = ''
+        }else if(value.name.length) {
+          todos.value = [...todos.value, value];
+        }
+   }
+
+   const deleteTask = (t) => {
+    todos.value = todos.value.filter((task) => task.name !== t.name);
+   };
+
+   const handleEdit = (task) => {
+     editTaskText.value = task.name
+   };
+
+   const filter = computed(() => {
+    return todos.value.filter(todo => todo.name.includes(searchValue.value));
+   })
+
+</script>
+
+<template>
+  <Card>
+    <template #title>
+       <div class="header">
+          Completed
+         <InputText type="text" placeholder="search..." v-model="searchValue" />
+       </div>
+    </template>
+    <template #content>
+      <Todo @handleAddNewTodo="handleAddTodo" :editTaskText="editTaskText"/>
+      <DataTable :value="filter" showGridlines >
+        <Column field="name" style="width: 30px; text-align: center;">
+          <template #header>
+               <i class="pi pi-check-circle"></i>
+          </template>
+          <template #body="{data}">
+            <Checkbox :model="data.completed" inputId="ingredient1" name="pizza" value="Cheese" />
+          </template>
+        </Column>
+        <Column field="name" header="Task"></Column>
+        <Column header="Action" style="width: 100px; text-align: center;">
+          <template #body="{ data }">
+             <div class="action">
+               <Button icon="pi pi-times" @click="deleteTask(data)" severity="danger" rounded></Button>
+               <Button icon="pi pi-pencil" @click="handleEdit(data)" severity="success" rounded></Button>
+             </div>
+          </template>
+        </Column>
+      </DataTable>
+    </template>
+  </Card>
+</template>
+
+<style>
+
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+
+ .action {
+  display: flex;
+  gap: 5px;
+ }
+
+ .pi-check-circle {
+  color: rgb(0, 189, 0);
+ }
+</style>
